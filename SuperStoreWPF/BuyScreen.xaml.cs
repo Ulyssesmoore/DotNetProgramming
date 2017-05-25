48fbe5f1cff20e30b0f34091b3e47678c5f9a5c4
Ulyssesmoore <sanderdebruin5c@gmail.com>
2017-05-25 17:23:02 +0200

    A beautiful and above all funtional buying screen. Type in the number of the amount of product you want and you will be able to buy it

--- SuperStoreWPF/BuyScreen.xaml.cs
+++ SuperStoreWPF/BuyScreen.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -32,7 +34,9 @@ namespace SuperStoreWPF
 
             SetupGrid();
 
-
+            currentBudget.Content = Convert.ToString(currentUser.Budget);
+            substraction.Content = "0.0";
+            budgetLeft.Content = "0.0";
 
             WindowStartupLocation = WindowStartupLocation.CenterScreen;
         }
@@ -65,10 +69,14 @@ namespace SuperStoreWPF
                 };
                 amountLabel.SetValue(Grid.ColumnProperty, 2);
                 amountLabel.SetValue(Grid.RowProperty, i);
+
                 TextBox amountInput = new TextBox();
-                amountInput.TextChanged += CheckText;
+                amountInput.PreviewTextInput += IsTextAllowed;
+                amountInput.TextChanged += GetSum;
+                DataObject.AddPastingHandler(amountInput, OnCancelCommand);
                 amountInput.SetValue(Grid.ColumnProperty, 3);
                 amountInput.SetValue(Grid.RowProperty, i);
+
                 selectProducts.Children.Add(nameLabel);
                 selectProducts.Children.Add(priceLabel);
                 selectProducts.Children.Add(amountLabel);
@@ -82,9 +90,38 @@ namespace SuperStoreWPF
             
         }
 
-        private void CheckText(object sender, RoutedEventArgs e)
+        private void IsTextAllowed(object sender, TextCompositionEventArgs e)
         {
+            Regex regex = new Regex("^[0-9]+$"); //regex that matches disallowed text
+            e.Handled = !regex.IsMatch(e.Text);
             
+        }
+
+        private void GetSum(object sender, TextChangedEventArgs e)
+        {
+            double totalprice = 0.0;
+            for (int i=0; i < selectProducts.RowDefinitions.Count;i++)
+            {
+                Label priceLabel = (Label) selectProducts.Children.Cast<UIElement>()
+                    .First(ef => Grid.GetColumn(ef) == 1 && Grid.GetRow(ef) == i);
+                TextBox tb = (TextBox)selectProducts.Children.Cast<UIElement>().First(ef => Grid.GetColumn(ef) == 3 && Grid.GetRow(ef) == i);
+                if (!string.IsNullOrWhiteSpace(tb.Text))
+                {
+                    totalprice += Convert.ToInt32(tb.Text) * Convert.ToDouble(priceLabel.Content);
+                }
+            }
+            Debug.WriteLine(totalprice);
+            substraction.Content = Convert.ToString(totalprice * -1);
+            BuyButton.IsEnabled = !((currentUser.Budget - totalprice) < 0.0);
+            notifications.Content = (currentUser.Budget - totalprice < 0.0) ? "You dont have enough budget for this purchase" : "No notifications";
+            budgetLeft.Content = Convert.ToString(currentUser.Budget - totalprice);
+        }
+
+
+
+        private void OnCancelCommand(object sender, DataObjectEventArgs e)
+        {
+            e.CancelCommand();
         }
     }
 }
